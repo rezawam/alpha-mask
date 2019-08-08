b63c8b7c9350e0aa6cdc0f1bd7cc7ad8fe4f5714
aegius <2004np@gmail.com>
2019-08-08 23:20:17 +0300

    oops

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function draw() {
 	var base = new Image();
 	base.onload = function() {
 		ctx.drawImage(base,0,0);
-		//ctx.globalCompositeOperation = 'destination-in';
+		ctx.globalCompositeOperation = 'destination-in';
 	};
 
 	num = 9
