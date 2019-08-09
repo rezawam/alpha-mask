57eb4c9210597c42c4f5b5462c1b60070028fb6a
aegius <2004np@gmail.com>
2019-08-09 11:03:26 +0300

    bug fixed

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-var count = 0
-
 function draw() {
 
-	// if (count >= 1) {
-	// 	//alert('жопа')
-	// } else {
-	// 	//alert(count);
+	const canvas = document.getElementById('canvas');
+	const ctx = canvas.getContext('2d');
 
-		const canvas = document.getElementById('canvas');
-		const ctx = canvas.getContext('2d');
-
-		canvas.width = canvas.width;
+	canvas.width = canvas.width; //refresh canvas
 
 	var base = new Image();
 	base.onload = function() {
 		ctx.drawImage(base,0,0);
-		ctx.globalCompositeOperation = 'destination-in';
 	};
 
 	var num = document.getElementById("myinput").value;
 	base.src = "bases/" + num + ".png";
 
 	var element = new Image();
-	element.onload = function test() {
+	function elem() {
+		ctx.globalCompositeOperation = 'destination-in';
 		ctx.drawImage(element,0,0);
 	};
 	element.src = "element.png";
 
-	count++;
-
-	// }
-
+	setTimeout(elem, 20);
 
 }
 
