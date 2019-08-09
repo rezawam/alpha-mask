932fe650d938e2d1e0b3d32373a7f5a0d3804619
aegius <2004np@gmail.com>
2019-08-09 21:45:36 +0300

    bug fixed

--- script.js
+++ script.js
@@ -5,22 +5,23 @@ function draw() {
 
 	canvas.width = canvas.width; //refresh canvas
 
+	var element = new Image();
+	element.onload = function elem() {
+		ctx.drawImage(element,0,0);
+	};
+	element.src = "element.png";
+
 	var base = new Image();
 	base.onload = function bas() {
+		ctx.globalCompositeOperation = 'source-in';
 		ctx.drawImage(base,0,0);
 	};
 
 	var num = document.getElementById("myinput").value;
 	base.src = "bases/" + num + ".png";
 
-	var element = new Image();
-	function elem() {
-		ctx.globalCompositeOperation = 'destination-in';
-		ctx.drawImage(element,0,0);
-	};
-	element.src = "element.png";
 
-	setTimeout(elem, 20);
+	//молимся ребят
 
 }
 
