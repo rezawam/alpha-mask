ad936f859de6c802ebee90a9231f5d46c927284d
nakti <naktiluka@gmail.com>
2019-08-10 01:10:23 +0300

    немного анимации и без миганий не ругайся плиз
    Анимация инициируется в первое нажатие и потом просто работает, но ему норм и так

--- script.js
+++ script.js
@@ -6,19 +6,35 @@ function draw() {
 	canvas.width = canvas.width; //refresh canvas
 
 	var element = new Image();
+	element.src = "element.png";
+	canvas.style.display = 'none';
+	canvas.start = "300";
 	element.onload = function elem() {
 		ctx.drawImage(element,0,0);
+		var base = new Image();
+		var num = document.getElementById("myinput").value;
+		base.src = "bases/" + num + ".png";
+		base.onload = function bas() {
+			ctx.globalCompositeOperation = 'source-in';
+			ctx.drawImage(base,0,0);
+			canvas.style.display = 'unset';
+		};
 	};
-	element.src = "element.png";
+	
+	var frame = 0;
+	var times = [100, 500, 1000, 42, 55];
+	var size = 15;
+	if (!canvas.start)
+		setTimeout(function anim() {
+			frame++;
+			if (frame >= times.length) {
+				frame = 0;
+			}
+			canvas.style.left = -frame * size + "px";
 
-	var base = new Image();
-	base.onload = function bas() {
-		ctx.globalCompositeOperation = 'source-in';
-		ctx.drawImage(base,0,0);
-	};
-
-	var num = document.getElementById("myinput").value;
-	base.src = "bases/" + num + ".png";
+			setTimeout(anim, times[frame]);
+		}, times[0]);
+	canvas.start = true;
 
 
 	//молимся ребят
